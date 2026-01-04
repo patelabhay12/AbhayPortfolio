c97fd12b5e53b0b88167557dca94856614f69eaf
Abhay patel <you@example.com>
2026-01-04 12:32:16 +0530

    * Profile pic fix for lg screen * profile pic fix for small screen

--- src/components/banner/RightBanner.js
+++ src/components/banner/RightBanner.js
@@ -5,7 +5,7 @@ const RightBanner = () => {
   return (
     <div className="w-full lgl:w-1/2 flex justify-center items-center relative">
       <img
-        className="w-[250px] h-[400px] lgl:w-[400px] lgl:h-[550px] z-10"
+        className="w-[300px] h-[400px] lgl:w-[400px] lgl:h-[450px] z-10"
         src={bannerImg}
         alt="bannerImg"
       />
